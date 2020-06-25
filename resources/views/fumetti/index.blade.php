055240c3d9ea8b1be1e7200a2a810e8fbe7f2d24
dnlx77 <dnlx77@gmail.com>
2020-06-25 14:00:00 +0200

    identazione codice

--- resources/views/fumetti/index.blade.php
+++ resources/views/fumetti/index.blade.php
@@ -19,10 +19,10 @@
     <tbody>
         @foreach ($fumetti AS $fumetto)
             <tr>
-                <td>{{ $fumetto->titolo}}</td>
+                <td>{{ $fumetto->titolo }}</td>
                 <td><a href="{{ route('fumetti.edit', $fumetto->id) }}">modifica</a></td>
-                <td>{{ $fumetto->created_at}}</td>
-                <td>{{ $fumetto->updated_at}}</td>
+                <td>{{ $fumetto->created_at }}</td>
+                <td>{{ $fumetto->updated_at }}</td>
             </tr>
         @endforeach
     </tbody>
