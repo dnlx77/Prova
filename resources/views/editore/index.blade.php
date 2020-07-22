a87f6c1015bed08863739f6d96851ab8ba3ebd52
dnlx77 <dnlx77@gmail.com>
2020-07-22 16:31:56 +0200

    fix editori

--- resources/views/editore/index.blade.php
+++ resources/views/editore/index.blade.php
@@ -7,10 +7,7 @@
         <tr>
             <th>Editore</th>
             <th>Modifica</th>
-<<<<<<< HEAD
             <th>Elimina</th>
-=======
->>>>>>> 1218a2316fc72960efc8a158051b7176abe57b37
             <th>Data inserimento</th>
             <th>Data aggiornamento</th>
         </tr>
@@ -20,10 +17,7 @@
             <tr>
                 <td>{{ $editore->nome }}</td>
                 <td><a href="{{ route('editore.edit', $editore->id) }}">modifica</a></td>
-<<<<<<< HEAD
                 <td><a href="{{ route('editore.elimina_form', $editore->id) }}">elimina</a></td>
-=======
->>>>>>> 1218a2316fc72960efc8a158051b7176abe57b37
                 <td>{{ $editore->created_at }}</td>
                 <td>{{ $editore->updated_at }}</td>
             </tr>
