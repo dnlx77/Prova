4865498189f47fdb339dd6edf3ba1da95ce7ffe4
dnlx77 <dnlx77@gmail.com>
2020-09-07 19:47:53 +0200

    inizio implementazione albo

--- resources/views/albo/form.blade.php
+++ resources/views/albo/form.blade.php
@@ -3,6 +3,14 @@
     <input type="text" class="form-control" name="num_pagine" value="{{ !empty(old('num_pagine')) ? old('num_pagine') : (!empty($albo->num_pagine) ? $albo->pagine : '') }}"/>
 </div>
 <div class="form-group">
-    <label for="titolo">Nome:</label>
-    <input type="text" class="form-control" name="nome" value="{{ !empty(old('nome')) ? old('nome') : (!empty($autore->nome) ? $autore->nome : '') }}"/>
+    <label for="barcode">Barcode:</label>
+    <input type="text" class="form-control" name="barcode" value="{{ !empty(old('barcode')) ? old('barcode') : (!empty($albo->barcode) ? $albo->barcode : '') }}"/>
+</div>
+<div class="form-group">
+    <label for="prezzo">Prezzo:</label>
+    <input type="number" class="form-control" name="prezzo" value="{{ !empty(old('prezzo')) ? old('prezzo') : (!empty($albo->prezzo) ? $albo->prezzo : '') }}"/>
+</div>
+<div class="form-group">
+    <label for="copertina">Copertina albo:</label>
+    <input type="file" class="form-control" name="copertina" value="{{ !empty(old('copertina')) ? old('copertina') : (!empty($albo->copertina) ? $albo->copertina : '') }}"/>
 </div>
