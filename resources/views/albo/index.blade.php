de5387c073abf011430569b5004bf4f741532a7a
dnlx77 <dnlx77@gmail.com>
2020-09-13 18:26:08 +0200

    visualizzazione immagine copertina

--- resources/views/albo/index.blade.php
+++ resources/views/albo/index.blade.php
@@ -8,6 +8,7 @@
             <th>Numero di pagine</th>
             <th>Prezzo</th>
             <th>Codice a barre</th>
+            <th>Copertina</th>
             <th>Modifica</th>
             <th>Elimina</th>
             <th>Data inserimento</th>
@@ -20,6 +21,14 @@
                 <td>{{ $albi->num_pagine }}</td>
                 <td>{{ $albi->prezzo }}</td>
                 <td>{{ $albi->barcode}}</td>
+                <td>
+                    <div class="card" style="width: 18rem;">
+                    <img src="{{ url('storage/'.$albi->filename) }}" class="card-img-top" alt="{{ $albi->filename }}">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </td>
                 <td><a href="{{ route('albo.edit', $albi->id) }}">modifica</a></td>
                 <td><a href="{{ route('albo.elimina_form', $albi->id) }}">elimina</a></td>
                 <td>{{ $albi->created_at }}</td>
