095e48fa369669ccbbc8c4de3a141f2e2170e6ad
dnlx77 <dnlx77@gmail.com>
2020-09-13 20:18:10 +0200

    modifica descrizione copertina

--- resources/views/albo/index.blade.php
+++ resources/views/albo/index.blade.php
@@ -25,7 +25,7 @@
                     <div class="card" style="width: 18rem;">
                     <img src="{{ url('storage/'.$albi->filename) }}" class="card-img-top" alt="{{ $albi->filename }}">
                         <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <p class="card-text">{{ $albi->original_filename }}</p>
                         </div>
                       </div>
                 </td>
