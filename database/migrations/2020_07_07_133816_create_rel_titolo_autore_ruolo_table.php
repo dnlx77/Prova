8c14c045f4e339be7f1722d915804b3f08abb229
mariosabatino <m.sabatino@gebsoftware.com>
2020-10-16 11:41:58 +0200

    fix foreign key

--- database/migrations/2020_07_07_133816_create_rel_titolo_autore_ruolo_table.php
+++ database/migrations/2020_07_07_133816_create_rel_titolo_autore_ruolo_table.php
@@ -16,7 +16,7 @@ class CreateRelTitoloAutoreRuoloTable extends Migration
         Schema::create('rel_titolo_autore_ruolo', function (Blueprint $table) {
             $table->bigInteger('titolo_id')->unsigned();
             $table->bigInteger('autore_id')->unsigned();
-            $table->bigInteger('ruolo_id')->nullable()->unsigned();
+            $table->bigInteger('ruolo_id')->unsigned();
             $table->timestamps();
             $table->primary(['titolo_id', 'autore_id', 'ruolo_id']);
         });
