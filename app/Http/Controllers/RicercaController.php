b3402f11ec360a0f2fa42dce60995e317f6747b6
dnlx77 <dnlx77@gmail.com>
2021-08-03 16:02:25 +0200

    script non funzionante

--- app/Http/Controllers/RicercaController.php
+++ app/Http/Controllers/RicercaController.php
@@ -21,8 +21,8 @@ class RicercaController extends Controller
         $cerca_in = $request->has('cerca_in') ? $request->get('cerca_in') : '';
         $search = $request->has('titolo') ? $request->get('titolo') : ''; 
         
-        $sort_by = 'created_at';
-        $order = 'desc';
+        $sort_by = 'numero';
+        $order = 'asc';
         $per_page = 10;
         $albi = Albo::search($search)->orderBy($sort_by, $order)->paginate($per_page);
 
--- resources/views/cerca/form.blade.php
+++ resources/views/cerca/form.blade.php
@@ -1,12 +1,20 @@
 <div class="form-group">
-    <label for="cerca">Cerca per:</label>
+    <label for="cerca">Cerca:</label>
     <select id="cerca-select" name="cerca_in">
-        <option value=""></option>
         @foreach ($lista_campi_ricerca as $current_campo)
             <option value="{{ $current_campo }}">{{ $current_campo }}</option>
         @endforeach
     </select>
 
-    <label for="titolo">Titolo albo da cercare:</label>
+    <label id="cerca-label" for="titolo">Titolo albo da cercare:</label>
     <input type="search" class="form-control" name="titolo"/>
 </div>
+
+<script>
+    $(document).ready(function(){
+        $("#cerca-select").on('change', function(){
+            
+            $("#cerca-label").val('prova');
+            $("#cerca-label").trigger('change');
+    });
+</script>
